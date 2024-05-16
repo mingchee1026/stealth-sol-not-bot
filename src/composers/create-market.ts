9fddc1d0ff24adadab5fbc842ff2f14f17abe89e
cheeming <dragon.steal75@gmail.com>
2024-05-16 09:46:08 +0900

    fix: update market

--- src/composers/create-market.ts
+++ src/composers/create-market.ts
@@ -4,9 +4,13 @@ import { Router } from '@grammyjs/router';
 import { Menu } from '@grammyjs/menu';
 
 import { MainContext } from '@root/configs/context';
-import { generateCreateMarketMessage } from './helpers';
+import { generateWelcomeMessage, generateCreateMarketMessage } from './helpers';
 import { getPrimaryWallet } from '@root/services/wallet-service';
-import { serviceSettings, serviceOpenmarket } from '@root/services';
+import {
+  serviceSettings,
+  serviceOpenmarket,
+  serviceToken,
+} from '@root/services';
 import { saveOpenmarket } from '@root/services/market-service';
 import { QUOTE_ADDRESS } from '@root/configs';
 import { command } from './constants';
@@ -29,7 +33,12 @@ const router = new Router<MainContext>((ctx) => ctx.session.step);
 const createMarketMenu = new Menu<MainContext>('create-market-menu')
   .text((ctx) => ctx.t('label-token-address'), inputBaseTokenCbQH)
   .row()
-  .text((ctx) => `--- ${ctx.t('label-quote-token')} ---`)
+  .text(
+    (ctx) => `--- ${ctx.t('label-quote-token')} ---`,
+    (ctx: any) => {
+      return false;
+    },
+  )
   .row()
   .text(
     async (ctx: any) => {
@@ -40,20 +49,20 @@ const createMarketMenu = new Menu<MainContext>('create-market-menu')
       if (ctx.session.createMarket.quoteMint !== 'SOL') {
         ctx.session.createMarket.quoteMint = 'SOL';
         const message = await generateCreateMarketMessage(ctx);
-        ctx.editMessageText(message);
+        ctx.editMessageText(message, { parse_mode: 'HTML' });
       }
     },
   )
   .text(
     async (ctx: any) => {
       const value = ctx.session.createMarket.quoteMint;
-      return `${value === 'USDT' ? '✅ ' : ''} USDT`;
+      return `${value === 'USDC' ? '✅ ' : ''} USDC`;
     },
     async (ctx: any) => {
-      if (ctx.session.createMarket.quoteMint !== 'USDT') {
-        ctx.session.createMarket.quoteMint = 'USDT';
+      if (ctx.session.createMarket.quoteMint !== 'USDC') {
+        ctx.session.createMarket.quoteMint = 'USDC';
         const message = await generateCreateMarketMessage(ctx);
-        ctx.editMessageText(message);
+        ctx.editMessageText(message, { parse_mode: 'HTML' });
       }
     },
   )
@@ -61,12 +70,91 @@ const createMarketMenu = new Menu<MainContext>('create-market-menu')
   .submenu((ctx) => ctx.t('label-lot-size'), 'custom-lot-menu')
   .submenu((ctx) => ctx.t('label-tick-size'), 'custom-tick-menu')
   .row()
-  .text((ctx) => ctx.t('label-advance-options'))
+  .text(
+    (ctx) => `--- ${ctx.t('label-advance-options')} ---`,
+    (ctx: any) => {
+      return false;
+    },
+  )
+  .row()
+  .text(
+    async (ctx: any) => {
+      const value = ctx.session.createMarket.marketPrice;
+      return `${value === 0.4 ? '✅ ' : ''} 0.4 SOL`;
+    },
+    async (ctx: any) => {
+      if (ctx.session.createMarket.marketPrice !== 0.4) {
+        ctx.session.createMarket.marketPrice = 0.4;
+        ctx.session.createMarket.eventLength = 128;
+        ctx.session.createMarket.requestLength = 9;
+        ctx.session.createMarket.orderbookLength = 201;
+        const message = await generateCreateMarketMessage(ctx);
+        ctx.editMessageText(message, { parse_mode: 'HTML' });
+      }
+    },
+  )
+  .text(
+    async (ctx: any) => {
+      const value = ctx.session.createMarket.marketPrice;
+      return `${value === 1.5 ? '✅ ' : ''} 1.5 SOL`;
+    },
+    async (ctx: any) => {
+      if (ctx.session.createMarket.marketPrice !== 1.5) {
+        ctx.session.createMarket.marketPrice = 1.5;
+        ctx.session.createMarket.eventLength = 1400;
+        ctx.session.createMarket.requestLength = 63;
+        ctx.session.createMarket.orderbookLength = 450;
+        const message = await generateCreateMarketMessage(ctx);
+        ctx.editMessageText(message, { parse_mode: 'HTML' });
+      }
+    },
+  )
+  .text(
+    async (ctx: any) => {
+      const value = ctx.session.createMarket.marketPrice;
+      return `${value === 2.8 ? '✅ ' : ''} 2.8 SOL`;
+    },
+    async (ctx: any) => {
+      if (ctx.session.createMarket.marketPrice !== 2.8) {
+        ctx.session.createMarket.marketPrice = 2.8;
+        ctx.session.createMarket.eventLength = 2978;
+        ctx.session.createMarket.requestLength = 63;
+        ctx.session.createMarket.orderbookLength = 909;
+        const message = await generateCreateMarketMessage(ctx);
+        ctx.editMessageText(message, { parse_mode: 'HTML' });
+      }
+    },
+  )
   .row()
   .text((ctx) => ctx.t('label-event-length'), inputEventLengthCbQH)
   .text((ctx) => ctx.t('label-request-length'), inputRequestLengthCbQH)
   .row()
   .text((ctx) => ctx.t('label-orderbook-length'), inputOrderbookLengthCbQH)
+  .row()
+  .back('🔙 Back', async (ctx) => {
+    const welcomeMessage = await generateWelcomeMessage(ctx);
+    await ctx.editMessageText(welcomeMessage, { parse_mode: 'HTML' });
+  })
+  .text(
+    (ctx) => '🗑 Clear Information',
+    async (ctx: MainContext) => {
+      const initialized = isInitialized(ctx);
+      if (initialized) {
+        return;
+      }
+
+      initSessionData(ctx);
+
+      try {
+        const message = await generateCreateMarketMessage(ctx);
+        ctx.editMessageText(message, { parse_mode: 'HTML' });
+      } catch (err) {
+        if (err) {
+          console.log(err);
+        }
+      }
+    },
+  )
   .row()
   .text((ctx) => ctx.t('label-create-openpool-market'), fireCreateMarketCbQH);
 // .row()
@@ -80,13 +168,13 @@ const customLotMenu = new Menu<MainContext>('custom-lot-menu')
   .back('100', async (ctx) => {
     ctx.session.createMarket.baseLogSize = 100;
     const message = await generateCreateMarketMessage(ctx);
-    ctx.editMessageText(message);
+    ctx.editMessageText(message, { parse_mode: 'HTML' });
   })
   .row()
   .back('1000', async (ctx) => {
     ctx.session.createMarket.baseLogSize = 1000;
     const message = await generateCreateMarketMessage(ctx);
-    ctx.editMessageText(message);
+    ctx.editMessageText(message, { parse_mode: 'HTML' });
   })
   .row()
   .text((ctx) => ctx.t('label-custom'), inputLotSizeCbQH)
@@ -97,13 +185,13 @@ const customTickMenu = new Menu<MainContext>('custom-tick-menu')
   .back('0.0000001', async (ctx) => {
     ctx.session.createMarket.tickSize = 0.0000001;
     const message = await generateCreateMarketMessage(ctx);
-    ctx.editMessageText(message);
+    ctx.editMessageText(message, { parse_mode: 'HTML' });
   })
   .row()
   .back('0.00000001', async (ctx) => {
     ctx.session.createMarket.tickSize = 0.00000001;
     const message = await generateCreateMarketMessage(ctx);
-    ctx.editMessageText(message);
+    ctx.editMessageText(message, { parse_mode: 'HTML' });
   })
   .row()
   .text((ctx) => ctx.t('label-custom'), inputTickSizeCbQH)
@@ -143,7 +231,7 @@ async function inputBaseTokenCbQH(ctx: MainContext) {
   try {
     ctx.session.step = Route.MARKET_BASE_TOKEN;
 
-    await ctx.reply(`Enter ${ctx.t('label-token-address')}:`, {
+    await ctx.reply(`Enter Token Address:`, {
       parse_mode: 'HTML',
       reply_markup: {
         force_reply: true,
@@ -158,13 +246,13 @@ async function inputLotSizeCbQH(ctx: MainContext) {
   try {
     ctx.session.step = Route.MARKET_LOT_SIZE;
 
-    await ctx.reply(`Enter ${ctx.t('label-lot-size')}:`, {
+    await ctx.reply(`Enter Minimum Buy (Order Size):`, {
       parse_mode: 'HTML',
       reply_markup: {
         force_reply: true,
       },
     });
-  } catch (err: any) {
+  } catch (err) {
     debug(err);
   }
 }
@@ -173,7 +261,7 @@ async function inputTickSizeCbQH(ctx: MainContext) {
   try {
     ctx.session.step = Route.MARKET_TICK_SIZE;
 
-    await ctx.reply(`Enter ${ctx.t('label-tick-size')}:`, {
+    await ctx.reply(`Enter Tick Size (Min Price Cgange):`, {
       parse_mode: 'HTML',
       reply_markup: {
         force_reply: true,
@@ -234,7 +322,45 @@ async function fireMarketInfomationRouteHandler(ctx: MainContext) {
     const text = ctx.msg!.text as string;
     switch (ctx.session.step) {
       case Route.MARKET_BASE_TOKEN:
-        ctx.session.createMarket.baseMint = text;
+        const market = await serviceOpenmarket.getOpenmarketByToken(
+          ctx.chat!.id,
+          text,
+        );
+
+        if (market) {
+          await ctx.reply('🔴 Token have already been used to create market.');
+          return;
+        }
+
+        const baseToken = await serviceToken.getMintToken(ctx.chat!.id, text);
+        if (!baseToken) {
+          await ctx.reply('🔴 Token information not found.');
+        } else {
+          ctx.session.createMarket.baseMint = text;
+          const supply = 90_000_000_000; //baseToken.supply;
+          if (supply <= 100_000) {
+            ctx.session.createMarket.baseLogSize = 0.01;
+            ctx.session.createMarket.tickSize = 0.0001;
+          } else if (supply <= 1_000_000) {
+            ctx.session.createMarket.baseLogSize = 0.1;
+            ctx.session.createMarket.tickSize = 0.00001;
+          } else if (supply <= 10_000_000) {
+            ctx.session.createMarket.baseLogSize = 1;
+            ctx.session.createMarket.tickSize = 0.000001;
+          } else if (supply <= 100_000_000) {
+            ctx.session.createMarket.baseLogSize = 10;
+            ctx.session.createMarket.tickSize = 0.0000001;
+          } else if (supply <= 1_000_000_000) {
+            ctx.session.createMarket.baseLogSize = 100;
+            ctx.session.createMarket.tickSize = 0.00000001;
+          } else if (supply <= 10_000_000_000) {
+            ctx.session.createMarket.baseLogSize = 1000;
+            ctx.session.createMarket.tickSize = 0.000000001;
+          } else if (supply <= 100_000_000_000) {
+            ctx.session.createMarket.baseLogSize = 10000;
+            ctx.session.createMarket.tickSize = 0.0000000001;
+          }
+        }
         break;
       case Route.MARKET_LOT_SIZE:
         ctx.session.createMarket.baseLogSize = Number(text);
@@ -243,13 +369,19 @@ async function fireMarketInfomationRouteHandler(ctx: MainContext) {
         ctx.session.createMarket.tickSize = Number(text);
         break;
       case Route.MARKET_EVENT_LENGTH:
-        ctx.session.createMarket.eventLength = Number(text);
+        const value1 = Number(text);
+        ctx.session.createMarket.eventLength =
+          value1 < 128 ? 128 : value1 > 2978 ? 2978 : value1;
         break;
       case Route.MARKET_REQUEST_LENGTH:
-        ctx.session.createMarket.requestLength = Number(text);
+        const value2 = Number(text);
+        ctx.session.createMarket.requestLength =
+          value2 < 9 ? 9 : value2 > 63 ? 63 : value2;
         break;
       case Route.MARKET_ORDERBOOK_LENGTH:
-        ctx.session.createMarket.orderbookLength = Number(text);
+        const value3 = Number(text);
+        ctx.session.createMarket.orderbookLength =
+          value3 < 201 ? 201 : value3 > 909 ? 909 : value3;
         break;
       default:
         break;
@@ -305,6 +437,16 @@ async function fireCreateMarketCbQH(ctx: MainContext) {
 
     if (!ctx.session.createMarket.baseMint) {
       await ctx.reply('🔴 Please enter Token Address.');
+      return;
+    }
+
+    const market = await serviceOpenmarket.getOpenmarketByToken(
+      ctx.chat!.id,
+      ctx.session.createMarket.baseMint,
+    );
+
+    if (market) {
+      await ctx.reply('🔴 Token have already been used to create market.');
       return;
     }
 
@@ -367,6 +509,8 @@ async function fireCreateMarketCbQH(ctx: MainContext) {
         ret.marketId,
         ret.txSignature,
       );
+
+      // initSessionData(ctx);
     }
   } catch (err: any) {
     const message = '🔴 Failed create market.';
@@ -379,3 +523,30 @@ async function fireCreateMarketCbQH(ctx: MainContext) {
     }
   }
 }
+
+const initSessionData = (ctx: MainContext) => {
+  ctx.session.createMarket.baseMint = '';
+  ctx.session.createMarket.quoteMint = 'SOL';
+  ctx.session.createMarket.baseLogSize = 0;
+  ctx.session.createMarket.tickSize = 0;
+  ctx.session.createMarket.marketPrice = 0.4;
+  ctx.session.createMarket.eventLength = 128;
+  ctx.session.createMarket.requestLength = 9;
+  ctx.session.createMarket.orderbookLength = 201;
+};
+
+const isInitialized = (ctx: MainContext) => {
+  if (
+    ctx.session.createMarket.baseMint !== '' ||
+    ctx.session.createMarket.quoteMint !== 'SOL' ||
+    ctx.session.createMarket.baseLogSize !== 0 ||
+    ctx.session.createMarket.tickSize !== 0 ||
+    ctx.session.createMarket.marketPrice !== 0.4 ||
+    ctx.session.createMarket.eventLength !== 128 ||
+    ctx.session.createMarket.requestLength !== 9 ||
+    ctx.session.createMarket.orderbookLength !== 201
+  ) {
+    return false;
+  }
+  return true;
+};
