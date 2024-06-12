1d92ca483ecbd148d5d875af6bc6d2da6350161a
cheeming <dragon.steal75@gmail.com>
2024-06-12 18:10:27 +0900

    fix: update token service

--- src/services/token-service.ts
+++ src/services/token-service.ts
@@ -1,9 +1,14 @@
 import { Wallet } from '@coral-xyz/anchor';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import FormData from 'form-data';
+import axios from 'axios';
+import * as fs from 'fs';
+import * as path from 'path';
 
-import { ENV, RPC_ENDPOINT_MAIN, RPC_ENDPOINT_DEV } from '@root/configs';
+import { ENV } from '@root/configs';
 import { Connectivity, CreateTokenInput } from '@root/web3';
 import { getKeypairFromStr } from '@root/web3/base/utils';
+import { deployDataToIPFS } from '@root/web3/base/utils';
 import { web3ErrorToStr } from '@root/web3/errors';
 import Token from '@root/models/token-model';
 import { chargeToSite } from './utils';
@@ -52,12 +57,26 @@ export async function getMintToken(chatId: number, mintAddress: string) {
   }
 }
 
+export const saveLogoImage = async (file_path: string, filePath: string) => {
+  try {
+    const fileUrl = `https://api.telegram.org/file/bot${ENV.BOT_TOKEN}/${file_path}`;
+    console.log('file Url', fileUrl);
+    const response = await axios.get(fileUrl, { responseType: 'stream' });
+    await response.data.pipe(fs.createWriteStream(filePath));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export async function mintToken(
   deployWallet: string,
   inputData: CreateTokenInput,
+  logoPath: string,
   solTxnsTip: number,
 ): Promise<{ address: string; tx: string; err: string }> {
-  const rpcEndpoint = ENV.IN_PRODUCTION ? RPC_ENDPOINT_MAIN : RPC_ENDPOINT_DEV;
+  const rpcEndpoint = ENV.IN_PRODUCTION
+    ? ENV.RPC_ENDPOINT_MAIN
+    : ENV.RPC_ENDPOINT_DEV;
   const walletkeyPair = getKeypairFromStr(deployWallet);
 
   if (!walletkeyPair || !walletkeyPair?.publicKey) {
@@ -66,6 +85,26 @@ export async function mintToken(
   }
 
   const wallet = new Wallet(walletkeyPair);
+
+  if (logoPath !== '') {
+    const fileData = fs.createReadStream(logoPath);
+    const logoFile = {
+      buffer: fileData,
+      name: path.basename(logoPath),
+      type: path.extname(logoPath.slice(1)),
+    };
+    const formData = new FormData();
+    formData.append('file', logoFile.buffer, {
+      filename: logoFile.name,
+      contentType: logoFile.type,
+    });
+
+    const ipfsHash = await deployDataToIPFS(formData, 'File');
+    if (ipfsHash) {
+      // inputData.image = `https://${ENV.PINATA_DOMAIN}/ipfs/${ipfsHash}`;
+      inputData.image = `https://ipfs.io/ipfs/${ipfsHash}`;
+    }
+  }
 
   const connectivity = new Connectivity({
     wallet: wallet,
@@ -78,12 +117,12 @@ export async function mintToken(
   try {
     const res = await connectivity.createToken(inputData);
 
-    if (res?.Err) {
-      return { address: '', tx: '', err: 'Token minting failed.' };
-    }
+    // if (!res || !res.Ok) {
+    //   return { address: '', tx: '', err: res.Err };
+    // }
 
-    if (!res || !res?.Ok) {
-      return { address: '', tx: '', err: 'Token minting failed.' };
+    if (res.Err) {
+      return { address: '', tx: '', err: res.Err };
     }
 
     console.log('Token successfully created.');
@@ -95,13 +134,14 @@ export async function mintToken(
     );
 
     return {
-      address: res.Ok.tokenAddress,
-      tx: res.Ok.txSignature,
+      address: res.Ok!.tokenAddress,
+      tx: res.Ok!.txSignature,
       err: '',
     };
   } catch (err) {
-    console.log(err);
-    return { address: '', tx: '', err: 'Token minting failed.' };
+    // console.log(err);
+    // return { address: '', tx: '', err: 'Token minting failed.' };
+    throw err;
   }
 
   // connectivity
